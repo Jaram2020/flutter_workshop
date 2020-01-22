34c7ca1737faf49e6cb4bbd96ed39916fe9a23e6
Etoile Jeon <wjstkdals409@naver.com>
2020-01-23 03:56:31 +0900

    data.dart revision

--- lib/data.dart
+++ lib/data.dart
@@ -1,9 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-QuerySnapshot getData(){
+List getData(){
   final Firestore fs = Firestore.instance;
+  var qls = [];
+  var ls = [];
   fs.collection("schedule").getDocuments().then((QuerySnapshot qss) {
-      qss.documents.forEach((f) => print('${f.data}}'));
-      return qss;
+    for(var i = 0; i < qss.documents.length; i++){
+      ls = [qss.documents[i]["date"], qss.documents[i]["topic"]];
+      qls.addAll(ls);
+    }
   });
+  return qls;
 }
