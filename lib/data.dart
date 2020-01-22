62cc12bf24dd1acd85fbccc14717ec7429d47eb7
Etoile Jeon <wjstkdals409@naver.com>
2020-01-22 22:26:56 +0900

    data.dart uploaded
    Firestore db to dart  list(consisted of DocumentSnapshot type data)

---
+++ lib/data.dart
@@ -0,0 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+QuerySnapshot getData(){
+  final Firestore fs = Firestore.instance;
+  fs.collection("schedule").getDocuments().then((QuerySnapshot qss) {
+      qss.documents.forEach((f) => print('${f.data}}'));
+      return qss;
+  });
+}
